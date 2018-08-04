1c42cb6118d56a344591739b14fdacb07a709e4a
Jim Mcbrayer <jimm@dogtopia.local>
2018-08-03 22:06:34 -0700

    adding full tests

---
+++ README.md
@@ -0,0 +1,25 @@
+# Feed Reader Testing
+ Jasmine testing for the feedreader app
+ This is an introduction to Jasmine Testing
+
+# Getting Started
+Clone and load up index.html
+
+# Dependiences
+Jasmine, jQuery, JS
+
+# Spec Tests
+1. Feeds Exist
+2. Feeds have a URL
+3. Feeds have a NAME
+4. Menu is hidden by default
+5. Menu changes visibility on click
+6. Feeds are loaded
+7. Feeds change content once loaded
+
+# Built With
+JavaScript/HTML/CSS/Jasmine
+
+# Author
+Jim Alan McBrayer
+
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,116 @@
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
+
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+$(function () {
+    
+    describe('RSS Feeds', function () {
+      // make sure all feeds actually exist by determining length greater than zero
+        it('are defined', function () {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+        // make sure all feeds has a url
+        it('have url', function () {
+            allFeeds.forEach(element => {
+                expect(element.url).toBeDefined();
+                expect(element.url.length).not.toBe(0);
+            });
+        });
+        // make sure all feeds have a name
+        it('has name', function () {
+            allFeeds.forEach(element => {
+                expect(element.name).toBeDefined();
+                expect(element.name.length).not.toBe(0);
+            });
+        });
+
+    });
+
+    describe('The menu', function () {
+        // make sure the menu is hidden by default
+        it('is hidden', function () {
+            expect(allFeeds.length).not.toBe(0);
+            expect(document.body.attributes[0].textContent).toBe('menu-hidden');
+        });
+        // make sure the menu changes when it's clicked, and goes back to hidden
+        it('changes visiblility', function () {
+            let link = document.getElementsByClassName('menu-icon-link');
+            link[0].click();
+            expect(document.body.attributes[0].textContent).toBe('');
+            link[0].click();
+            expect(document.body.attributes[0].textContent).toBe('menu-hidden');
+
+        });
+
+    });
+
+
+
+    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function () {
+        // 
+        beforeEach(function (done) {
+            loadFeed(0, function () {
+                done();
+            });
+        });
+        // make sure feeds were loaded by testing to determine length is greater than 0 for the entries
+        it('loaded feeds', function (done) {
+            let feeds = document.getElementsByClassName('entry');
+            expect(feeds.length).not.toBe(0);
+            done();
+        });
+        //Test with incorrect link
+
+    });
+
+    describe('News Feed Selection', function () {
+
+        // load each feed, store the text content in "content", then load the second feed and store the content in "newcontent" and then
+        // test to make sure they are not the same text content 
+        let content
+        let newcontent
+
+        beforeEach(function (done) {
+            loadFeed(0, function () {
+                content = document.getElementsByClassName('feed')[0].children[0].textContent
+                done();
+            });
+        });
+        beforeEach(function (done) {
+            loadFeed(1, function () {
+                newcontent = document.getElementsByClassName('feed')[0].children[0].textContent
+                done();
+            });
+        });
+        it('actually changes', function (done) {
+            expect(newcontent).not.toBe(content);
+            done();
+        });
+        beforeEach(function (done) {
+            loadFeed(2, function () {
+                content = document.getElementsByClassName('feed')[0].children[0].textContent
+                done();
+            });
+        });
+        beforeEach(function (done) {
+            loadFeed(3, function () {
+                newcontent = document.getElementsByClassName('feed')[0].children[0].textContent
+                done();
+            });
+        });
+        it('changes again', function (done) {
+            expect(newcontent).not.toBe(content);
+            loadFeed(0);
+            done();
+        });
+    });
+    
+}());
